e792e3495903eb8efb8ae9b9e0c69ecea7cb9bf8
ahll-TOSH\ahll <ahll11@hotmail.com>
2019-12-13 20:23:09 +0100

    first commit

---
+++ home.data.js
@@ -0,0 +1,176 @@
+"use strict";
+
+var HomeData = {
+  "es_ES": {
+    "title": "The Wine Road",
+    "showcase": {
+      "title": "Tu camino de vinos a China",
+      "content": "No importa Cantidad, Tipo, Localidad, Siempre los distribuimos a China",
+      "button": "Suministro Vinos"
+    },
+    "marcket": {
+      "title": "¡Te abrimos el mercado más grande del mundo!",
+      "data": [{
+        "like": "",
+        "share": "",
+        "image": "huge-marcket.jpg",
+        "title": "El mega mercado",
+        "content": "1.4 litros del vino por un año por cada chino, y va a crecer 39,97% en el año 2021"
+      }, {
+        "like": "",
+        "share": "",
+        "image": "like-wine.jpg",
+        "title": "El gusto chino",
+        "content": "Los chinos les gustan el vino, si, les gustan mucho                              "
+      }, {
+        "like": "",
+        "share": "",
+        "image": "spain-importation.jpg",
+        "title": "España arriba",
+        "content": "LLevamos nuestro vinos a China, somos mejores. Y podemos crecer mucho               "
+      }]
+    },
+    "platform": {
+      "title": "El camino más rápido y fiable al mercado China",
+      "data": [{
+        "avatar": "",
+        "title": "No papeleos",
+        "name": "Todo online y automatizado                 "
+      }, {
+        "avatar": "",
+        "title": "No traductores",
+        "name": "Hablamos por ti                            "
+      }, {
+        "avatar": "",
+        "title": "No intermediarios",
+        "name": "Te conectamos directamente a China         "
+      }, {
+        "avatar": "",
+        "title": "No perdidas",
+        "name": "Te vigilamos todos tus productos hasta China"
+      }]
+    },
+    "support": {
+      "title": "¡Conocimientos, Expertos y Tecnologías!",
+      "data": [{
+        "image": "",
+        "content": "Te analizamos el mercado, te integramos al mercado china",
+        "title": "Expertos en el mercado chino",
+        "share": "",
+        "like": ""
+      }, {
+        "image": "",
+        "content": "Elimino las barreras del mercado, te sientes como en españa",
+        "title": "Expertos en normas de comercio internacinal",
+        "share": "",
+        "like": ""
+      }, {
+        "image": "",
+        "content": "Usamos tecnologías y nuestro talento, y simplicamos el camino al mercado china",
+        "title": "Expertos en el tecnología",
+        "share": "",
+        "like": ""
+      }]
+    },
+    "contact": {
+      "title": "The wine road - Treasure ship group",
+      "data": {
+        "contact": "",
+        "phone": "Teléfono: +34 XXXXXXX",
+        "whatsapp": "Whatsapp: +34 XXXXXXX",
+        "wechat": "Wechat: +34 XXXXXXX",
+        "mail": "E-mail: "
+      }
+    },
+    "footer": {
+      "content": "Create, Design and Copy right reserved by",
+      "company": "The wine road - treasure ship group © 2019"
+    }
+  },
+  "zh_CN": {
+    "title": "红酒之路",
+    "showcase": {
+      "title": "红酒之路 - 来自伊比利亚的奢华",
+      "content": "婚庆，聚会，在家， 生活的必须",
+      "button": "咨询批发"
+    },
+    "marcket": {
+      "title": "拥抱西班牙的美酒吧",
+      "data": [{
+        "like": "",
+        "share": "",
+        "image": "huge-marcket.jpg",
+        "title": "我们的市场在增长",
+        "content": "人均 1.4 升, 39,97% 的增长 2021年, 不要错过机遇"
+      }, {
+        "like": "",
+        "share": "",
+        "image": "like-wine.jpg",
+        "title": "我们喜欢红酒",
+        "content": "婚庆, 聚会, 家居, 红酒都是我们的好伙伴"
+      }, {
+        "like": "",
+        "share": "",
+        "image": "spain-importation.jpg",
+        "title": "西班牙的品质",
+        "content": "当年奢华的哈布斯堡王朝, 古老的罗马品位, 现在西班牙的品质"
+      }]
+    },
+    "platform": {
+      "title": "我们专业的平台",
+      "data": [{
+        "avatar": "",
+        "title": "无须繁琐的手续",
+        "name": "在线, 自动, 随叫随到                 "
+      }, {
+        "avatar": "",
+        "title": "无须当心品质",
+        "name": "我们带来西班牙酒厂的直销                           "
+      }, {
+        "avatar": "",
+        "title": "没有层层剥削",
+        "name": "我们直接联系你到西班牙         "
+      }, {
+        "avatar": "",
+        "title": "没有资质的要求",
+        "name": "只需轻轻的一点, 你就能经营来自西班牙的各种品牌"
+      }]
+    },
+    "support": {
+      "title": "专业, 正宗, 创新",
+      "data": [{
+        "image": "",
+        "content": "我们为你带来真正的红酒",
+        "title": "真正来自西班牙的品质, 欧洲的享受",
+        "share": "",
+        "like": ""
+      }, {
+        "image": "",
+        "content": "我们为你打破贸易的壁垒",
+        "title": "用平台解决一切繁琐的制度和障碍",
+        "share": "",
+        "like": ""
+      }, {
+        "image": "",
+        "content": "我们用科技, 知识, 智慧, 创造红酒之路",
+        "title": "用我们的知识,技术和态度打造新的贸易",
+        "share": "",
+        "like": ""
+      }]
+    },
+    "contact": {
+      "title": "",
+      "data": {
+        "contact": "联系人: ",
+        "phone": "电话:",
+        "whatsapp": "Whatsapp:",
+        "wechat": "微信:",
+        "mail": "邮箱: "
+      }
+    },
+    "footer": {
+      "content": "创建，设计，版权 归属于 ",
+      "company": " 红酒之路- 宝船集团© 2019"
+    }
+  }
+};
---
+++ home.js
@@ -0,0 +1,220 @@
+"use strict";
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+var text = function text() {
+  return HomeData[Locale];
+};
+
+var imageUrl = ImageSource + "/home/image/";
+
+function ShowCase(props) {
+  return React.createElement("section", {
+    id: "showcase",
+    className: "showcase background-wine text-white"
+  }, React.createElement("div", {
+    className: ""
+  }, React.createElement("img", {
+    src: imageUrl + "showcase.jpg"
+  })), React.createElement("div", {
+    className: "text"
+  }, React.createElement("h1", null, text().showcase.title), React.createElement("div", null, React.createElement("p", {
+    className: "animateOne animated zoomIn"
+  }, text().showcase.content), React.createElement(Button, {
+    size: "small",
+    href: "#contact",
+    className: "text-white"
+  }, text().showcase.button))));
+}
+
+function MarcketCard(props) {
+  var data = props.data;
+  return React.createElement("div", {
+    className: "item background-wine marcket-card"
+  }, React.createElement(Card, null, React.createElement(CardMedia, {
+    className: "itemMedia",
+    image: imageUrl + data.image,
+    title: data.title
+  }), React.createElement(CardContent, null, React.createElement(Typography, {
+    gutterBottom: true,
+    variant: "headline",
+    component: "h2"
+  }, data.title), React.createElement(Typography, {
+    component: "p"
+  }, data.content)), React.createElement(CardActions, null)));
+}
+
+function Marcket(props) {
+  var cards = text().marcket.data.map(function (data, index) {
+    return React.createElement(MarcketCard, {
+      key: index,
+      data: data
+    });
+  });
+  return React.createElement("section", {
+    id: "marcket",
+    className: ""
+  }, React.createElement("h1", null, text().marcket.title), React.createElement("div", {
+    className: "itemList"
+  }, cards));
+}
+
+function PlatformCard(props) {
+  var data = props.data;
+  return React.createElement("div", {
+    className: "smallItem platform-card"
+  }, React.createElement(Card, null, React.createElement(CardHeader, {
+    avatar: React.createElement(Avatar, {
+      "aria-label": "Recipe",
+      src: data.avatar
+    }),
+    title: data.title,
+    subheader: data.profession
+  }), React.createElement(CardContent, null, React.createElement(Typography, {
+    gutterBottom: true,
+    variant: "headline",
+    component: "h2"
+  }, data.name), React.createElement(Typography, {
+    component: "p"
+  }, data.content)), React.createElement(CardActions, null, React.createElement(IconButton, {
+    href: "mailto:" + data.email,
+    alt: data.email
+  }))));
+}
+
+function Platform(props) {
+  var cards = text().platform.data.map(function (data, index) {
+    return React.createElement(PlatformCard, {
+      key: index,
+      data: data
+    });
+  });
+  return React.createElement("section", {
+    id: "platform",
+    className: "background-wine text-white"
+  }, React.createElement("h1", null, text().platform.title), React.createElement("div", {
+    className: "itemList"
+  }, cards));
+}
+
+function SupportCard(props) {
+  var data = props.data;
+  return React.createElement("div", {
+    className: "smallItem background-wine support-card"
+  }, React.createElement(Card, null, React.createElement(CardMedia, {
+    className: "itemMedia",
+    image: imageUrl + data.image,
+    title: data.title
+  }), React.createElement(CardContent, null, React.createElement(Typography, {
+    gutterBottom: true,
+    variant: "headline",
+    component: "h2"
+  }, data.title), React.createElement(Typography, {
+    component: "p"
+  }, data.content)), React.createElement(CardActions, null)));
+}
+
+function Support(props) {
+  var cards = text().support.data.map(function (data, index) {
+    return React.createElement(SupportCard, {
+      key: index,
+      data: data
+    });
+  });
+  return React.createElement("section", {
+    id: "support",
+    className: ""
+  }, React.createElement("h1", null, text().support.title), React.createElement("div", {
+    className: "itemList"
+  }, cards));
+}
+
+function Contact(props) {
+  var data = text().contact.data;
+  return React.createElement("section", {
+    id: "contact",
+    className: "background-wine text-white"
+  }, React.createElement("h1", null, data.contact), React.createElement("div", {
+    className: "contact-info"
+  }, React.createElement("div", {
+    className: "contact-info-item"
+  }, data.phone), React.createElement("div", {
+    className: "contact-info-item"
+  }, data.mail), React.createElement("div", {
+    className: "contact-info-item"
+  }, data.whatsapp), React.createElement("div", {
+    className: "contact-info-item"
+  }, data.wechat)), React.createElement("div", {
+    className: "iframe"
+  }, React.createElement("iframe", {
+    className: "contact-map",
+    width: "100%",
+    height: "500px",
+    frameBorder: "0",
+    src: "",
+    allowFullScreen: true
+  })));
+}
+
+function Footer(props) {
+  var data = text().footer;
+  var listItem = [];
+
+  for (var name in data) {
+    listItem.push(React.createElement("p", null, data[name]));
+  }
+
+  return React.createElement("section", {
+    id: "footer",
+    className: "footer background-wine text-white"
+  }, listItem);
+}
+
+var Home =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(Home, _React$Component);
+
+  function Home(props) {
+    var _this;
+
+    _classCallCheck(this, Home);
+
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(Home).call(this, props));
+    _this.state = {
+      locale: Locale
+    };
+    _this.changeTitle = props.title;
+    return _this;
+  }
+
+  _createClass(Home, [{
+    key: "render",
+    value: function render() {
+      this.changeTitle(text().title);
+      return React.createElement("div", null, React.createElement(ShowCase, null), React.createElement(Marcket, null), React.createElement(Platform, null), React.createElement(Support, null), React.createElement(Contact, null), React.createElement(Footer, null));
+    }
+  }]);
+
+  return Home;
+}(React.Component);
+
+window.onload = function () {
+  ReactDOM.render(React.createElement(Page, null, React.createElement(Home, null)), document.getElementById("page"));
+};
